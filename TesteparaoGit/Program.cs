fbec9f0535b420085056e09db60ff3bee9739691
Luanderson Ferreira <luandersonferreira123@gmail.com>
2020-11-03 15:52:39 -0300

    Projeto iniciado, mostrando a mensagem ola mundo

---
+++ TesteparaoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteparaoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
